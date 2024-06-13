b453d8b1514b595cbd58d3cf2d75faf72acd12a0
Benjamin Desole <benjamin.desole@gmail.com>
2024-06-13 23:31:28 +0200

    display movie cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  createTheme,
+  Grid,
+  LoadingOverlay,
+  MantineProvider,
+} from "@mantine/core";
+import { MovieCard } from "./components/movie-card/MovieCard";
+import "@mantine/core/styles.css";
+import "./App.css";
+import { movies$ } from "./movies/movies";
+import { Movie } from "./types";
+
+const theme = createTheme({
+  headings: {
+    fontFamily: "Georgia, serif",
+  },
+  spacing: {
+    xs: "0.5rem",
+    sm: "0.75rem",
+    md: "1rem",
+    lg: "1.5rem",
+    xl: "2rem",
+  },
+});
 
 function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    movies$.then((movies) => setMovies(movies));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider defaultColorScheme="light" theme={theme}>
+      <Grid>
+        <LoadingOverlay
+          visible={movies.length === 0}
+          overlayProps={{ blur: 2 }}
+          loaderProps={{ type: "bars" }}
+        />
+        {movies.map((movie) => (
+          <Grid.Col key={`movie-${movie.id}`} span={{ base: 6, md: 4, lg: 2 }}>
+            <MovieCard movie={movie} />
+          </Grid.Col>
+        ))}
+      </Grid>
+    </MantineProvider>
   );
 }
 
